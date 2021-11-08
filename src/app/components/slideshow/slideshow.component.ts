317082336d4171d70e50b4145f4520104baee592
Salomon <13030564@itcelaya.edu.mx>
2021-11-08 11:38:34 -0600

    Video.252 - Swiper - Slideshow de peliculas

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Swiper } from 'swiper';
 import { Movie } from '../../interfaces/cartelera-response';
 
 @Component({
@@ -6,10 +7,36 @@ import { Movie } from '../../interfaces/cartelera-response';
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
   // Input to get data property "movies" from component "pages/home" 
   @Input () movies?: Movie[];
   constructor() { }
+
+  ngAfterViewInit(): void {
+    const swiper = new Swiper('.swiper', {
+      // Optional parameters
+      // direction: 'vertical',
+      loop: true,
+    
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+    // Otra coas
+    // throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
     console.log(this.movies);
