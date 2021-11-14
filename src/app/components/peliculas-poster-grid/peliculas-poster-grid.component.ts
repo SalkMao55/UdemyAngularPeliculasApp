11f6eceb0157628629ebd593c519733369550f52
Salomon <13030564@itcelaya.edu.mx>
2021-11-14 14:03:45 -0600

    Video.256 - Calificacion de Estrellas

--- src/app/components/peliculas-poster-grid/peliculas-poster-grid.component.ts
+++ src/app/components/peliculas-poster-grid/peliculas-poster-grid.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response';
+import { StarRatingComponent } from 'ng-starrating';
 
 @Component({
   selector: 'app-peliculas-poster-grid',
@@ -15,6 +16,13 @@ export class PeliculasPosterGridComponent implements OnInit {
   
   ngOnInit(): void {
     console.log(this.movies);
+  }
+
+  onRate($event:{oldValue:number, newValue:number, starRating:StarRatingComponent}) {
+    alert(`Old Value:${$event.oldValue}, 
+      New Value: ${$event.newValue}, 
+      Checked Color: ${$event.starRating.checkedcolor}, 
+      Unchecked Color: ${$event.starRating.uncheckedcolor}`);
   }
 
 }
