eb8e445a750aa08bdcd14b9362ac1bac37854e22
Salomon <13030564@itcelaya.edu.mx>
2021-11-18 22:14:43 -0600

    Video.257 - InfiniteScroll de peliculas

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { PeliculasService } from '../../services/peliculas.service';
 import { Movie } from '../../interfaces/cartelera-response';
 
@@ -9,8 +9,26 @@ import { Movie } from '../../interfaces/cartelera-response';
 })
 export class HomeComponent implements OnInit {
 
-  // Array for save movies and use them inside the slideshow
+  // Array for movies on infinity Scroll
   public movies : Movie[] = [];
+  // Array for save movies and use them inside the slideshow
+  public moviesSlideshow: Movie[] = [];
+
+  // Interface for HostListener and implement the infinity scroll
+  @HostListener('window:scroll',['$event'])
+  onScroll(){
+    // With the interface every time we do scroll on the window, we activate this function
+    const max = ( document.documentElement.scrollHeight || document.body.scrollHeight ); // Get Max Scroll value
+    const pos = ( document.documentElement.scrollTop || document.body.scrollTop ) + (max/3); // Get current Scroll value
+    
+    if ( pos >= max) {
+      // TODO: llamar el servicio
+      this.peliculasService.getCartelera().subscribe( resp => {
+        this.movies.push(...resp.results); // Add every result to Array of movies
+      } );
+    }
+  }
+
   constructor( private peliculasService: PeliculasService ) { 
    }
 
@@ -18,6 +36,7 @@ export class HomeComponent implements OnInit {
     this.peliculasService.getCartelera().subscribe(resp => {
       // console.log(resp.results);
       this.movies = resp.results;
+      this.moviesSlideshow = resp.results;
     });
   }
 
--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,17 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { CarteleraResponse } from '../interfaces/cartelera-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeliculasService {
+  
+  // Properties
+  private baseUrl: string = 'https://api.themoviedb.org/3'; // Base URL API
+  private carteleraPage = 1; // To specify catelera page
 
   constructor( private http: HttpClient) { }
 
+  // Params structure to add in link of http service
+  // All properties have to be strings.
+  public get params() {
+    return {
+      api_key: '610c1b4839a0c0a384c5463bc55d2367',
+      language: 'es-MX',
+      page: this.carteleraPage.toString(),
+      region: 'MX'
+    }
+  }
+  
+
   getCartelera():Observable<CarteleraResponse>{
-    return this.http.get<CarteleraResponse>(`https://api.themoviedb.org/3/movie/now_playing?api_key=610c1b4839a0c0a384c5463bc55d2367&language=es-MX&page=1&region=MX`);
+    return this.http.get<CarteleraResponse>(`${this.baseUrl}/movie/now_playing`,{
+      params: this.params
+    }).pipe(
+      tap( ()=> {
+        this.carteleraPage += 1;
+      } )
+    );
   }
 
 }
