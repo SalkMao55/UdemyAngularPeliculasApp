c3e19ffc4287108f9802c9c20bc18eec17ce7ba0
Salomon <13030564@itcelaya.edu.mx>
2021-12-27 11:06:03 -0600

    Video.263 - Disenio de la pantalla de detalle

--- src/app/pages/pelicula/pelicula.component.ts
+++ src/app/pages/pelicula/pelicula.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+import { MovieResponse } from 'src/app/interfaces/movie-response';
 import { PeliculasService } from 'src/app/services/peliculas.service';
+import { StarRatingComponent } from 'ng-starrating';
+// This gives all the User Location Information
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-pelicula',
@@ -9,14 +14,36 @@ import { PeliculasService } from 'src/app/services/peliculas.service';
 })
 export class PeliculaComponent implements OnInit {
 
+  //Propertie use to show Movie information on Template "pelicula.component.html"
+  public declare pelicula: MovieResponse;
+  // We use "declare" to indicate the next information to compiler: "There is a property 
+  //          called name of type string. I shouldn't have to prove to you that name actually exists, 
+  //          but I want to use it anyway."
+  //Source: https://stackoverflow.com/questions/67351411/what-s-the-difference-between-definite-assignment-assertion-and-ambient-declarat
+
   constructor( private activatedRoute: ActivatedRoute,
-               private peliculasService: PeliculasService ) { }
+               private peliculasService: PeliculasService,
+               private location: Location ) { }
 
   ngOnInit(): void {
     const  {id}  = this.activatedRoute.snapshot.params;
     this.peliculasService.getPeliculaDetalle(id).subscribe( movie => {
       console.log(movie);
+      // Get Movie information from "pelicula.service.ts"
+      this.pelicula = movie;
     });
   }
-
+  
+  // To assing value of starts on movie, only show an Alert with the value indicated
+  onRate($event:{oldValue:number, newValue:number, starRating:StarRatingComponent}) {
+    alert(`Old Value:${$event.oldValue}, 
+      New Value: ${$event.newValue}, 
+      Checked Color: ${$event.starRating.checkedcolor}, 
+      Unchecked Color: ${$event.starRating.uncheckedcolor}`);
+  }
+  
+  // Come back to home page.
+  onRegresar(){
+    this.location.back();
+  }
 }
